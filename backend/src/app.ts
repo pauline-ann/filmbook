a3d7c767de63e541ad0709e08d1e90d52da8c325
pauline-ann <paulinebantayan@gmail.com>
2023-04-26 15:03:38 -0700

    improve error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
 import postsRoutes from './routes/posts'
 import morgan from 'morgan'
+import createHttpError, { isHttpError } from 'http-errors'
 
 const app = express() // init express server
 
@@ -14,8 +15,7 @@ app.use('/api/posts', postsRoutes)
 
 // handle access to nonexistent endpoint
 app.use((req, res, next) => {
-    // TODO res.status(404)
-    next(Error("Endpoint not found."))
+    next(createHttpError(404, "Endpoint not found"))
 })
 
 // error handler
@@ -23,10 +23,12 @@ app.use((req, res, next) => {
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error)
     let errorMessage = "An unknown error occurred."
-    if (error instanceof Error) {
+    let statusCode = 500
+    if (isHttpError(error)) {
+        statusCode = error.status
         errorMessage = error.message
     }
-    res.status(500).json({ error: errorMessage })
+    res.status(statusCode).json({ error: errorMessage })
 })
 
 export default app
--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express"
 import PostModel from '../models/post'
+import createHttpError from "http-errors"
+import mongoose from "mongoose"
 
 export const getPosts: RequestHandler = async (req, res, next) => {
     try {
@@ -15,7 +17,16 @@ export const getPost: RequestHandler = async (req, res, next) => {
     const postId = req.params.postId
 
     try {
+        if (!mongoose.isValidObjectId(postId)) {
+            throw createHttpError(400, 'Invalid post id.')
+        }
+
         const post = await PostModel.findById(postId).exec()
+
+        if (!post) {
+            throw createHttpError(404, "Post not found.")
+        }
+
         res.status(200).json(post)
     }
     catch (error) {
@@ -23,10 +34,26 @@ export const getPost: RequestHandler = async (req, res, next) => {
     }
 }
 
-export const createPost: RequestHandler = async (req, res, next) => {
+interface CreatePostBody {
+    fileName?: string,
+    file?: {
+        data: Buffer,
+        contentType: string
+    }
+    caption?: string
+}
+
+export const createPost: RequestHandler<unknown, unknown, CreatePostBody, unknown> = async (req, res, next) => {
+    const file = req.body.file
+    const fileName = req.body.fileName
     const caption = req.body.caption
 
     try {
+        // TODO
+        // if (!file || !fileName) {
+        //     throw createHttpError(400, "Post must have an image.")
+        // }
+
         const newPost = await PostModel.create({
             caption: caption
         })
