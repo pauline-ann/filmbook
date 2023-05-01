dbff93e288976027a10640f199dbd616af653930
pauline-ann <paulinebantayan@gmail.com>
2023-05-01 11:24:11 -0700

    close icon

--- client/src/components/modals/CreatePostModal.tsx
+++ client/src/components/modals/CreatePostModal.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import filmPhoto from '../../assets/film1.jpg'
 import LoadingIcon from '../icons/LoadingIcon'
-import { createPost } from '../../utils/createPost'
+import { createPost } from '../../network/createPost'
+import CloseIcon from '../icons/CloseIcon'
 
 interface CreatePostProps {
     toggleModal: (arg: boolean) => void
@@ -46,8 +47,7 @@ const CreatePost = ({ toggleModal }: CreatePostProps) => {
                                 className="absolute top-3 right-2.5 text-gray-700 bg-transparent hover:bg-gray-300 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                                 onClick={() => toggleModal(false)}
                             >
-                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                <span className="sr-only">Close modal</span>
+                                <CloseIcon />
                             </button>
                         </div>
                         {/*body*/}
