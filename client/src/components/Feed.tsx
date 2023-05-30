ba6542dcfb261d649fe91df0458950dcb097aa0c
pauline-ann <paulinebantayan@gmail.com>
2023-05-29 22:50:49 -0700

    link feed post to post page

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,5 +1,6 @@
 import { Post as PostModel } from "../models/post"
-import Post from "./Post"
+import FeedPost from "./FeedPost"
+import { Link } from "react-router-dom"
 
 interface FeedProps {
     posts?: PostModel[]
@@ -10,7 +11,9 @@ const Feed = ({ posts }: FeedProps) => {
     return (
         <div className='w-100 md:w-2/3 lg:w-1/2 space-y-12 pb-10'>
             {posts?.map((post) => (
-                <Post post={post} key={post._id} />
+                <Link to={`username/${post._id}`} className="flex">
+                    <FeedPost post={post} key={post._id} />
+                </Link>
             ))
             }
         </div>
