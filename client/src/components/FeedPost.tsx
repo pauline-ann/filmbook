7c334689acd33d33df5e0a4133231a71cce36f50
pauline-ann <paulinebantayan@gmail.com>
2023-05-29 22:52:51 -0700

    improve feed post component

---
+++ client/src/components/FeedPost.tsx
@@ -0,0 +1,34 @@
+import { Post as PostModel } from "../models/post"
+import filmPhoto from '../assets/film1.jpg'
+import { formatDate } from "../utils/formatData"
+
+interface PostProps {
+    post: PostModel
+}
+
+const Post = ({ post }: PostProps) => {
+    const {
+        caption,
+        updatedAt,
+        createdAt
+    } = post
+
+    const dateText = updatedAt > createdAt ? formatDate(updatedAt) : formatDate(createdAt)
+
+    return (
+        <div className="p-7 bg-white rounded-md shadow-md border border-gray-200 dark:bg-gray-100 dark:border-gray-200 flex flex-col">
+            <div className="relative">
+                <img className="lg" src={filmPhoto} alt="" />
+                <div className="p-5 flex flex-col flex-items justify-between">
+                    <p className="font-normal text-gray-900 mb-3 dark:text-gray-400">{caption}</p>
+                    <div className="flex justify-between">
+                        <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">@user</p>
+                        <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">{dateText}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
