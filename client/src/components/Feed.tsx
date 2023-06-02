56586cb7f63e7674c98bb7e4dd3ec049f4b7ed37
pauline-ann <paulinebantayan@gmail.com>
2023-06-01 17:40:30 -0700

    add key

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -11,8 +11,8 @@ const Feed = ({ posts }: FeedProps) => {
     return (
         <div className='w-100 md:w-2/3 lg:w-1/2 space-y-12 pb-10'>
             {posts?.map((post) => (
-                <Link to={`username/${post._id}`} className="flex">
-                    <FeedPost post={post} key={post._id} />
+                <Link to={`username/${post._id}`} className="flex" key={post._id}>
+                    <FeedPost post={post} />
                 </Link>
             ))
             }
