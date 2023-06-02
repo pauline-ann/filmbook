1a19e1a5d80bed83b26359d1f80d763207df085b
pauline-ann <paulinebantayan@gmail.com>
2023-06-01 17:41:06 -0700

    fix svg

--- client/src/components/icons/PlusIcon.tsx
+++ client/src/components/icons/PlusIcon.tsx
@@ -1,12 +1,13 @@
 interface PlusIconProps {
     width?: number
     color?: string
+    inline?: boolean
 }
 
-const PlusIcon = ({ width = 40, color = 'rgb(75 85 99)' }: PlusIconProps) => {
+const PlusIcon = ({ width = 40, color = 'rgb(75 85 99)', inline }: PlusIconProps) => {
 
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={width} height={width} fill={color} className="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" /> </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width={width} height={width} fill={color} className={`bi bi-plus ${inline ? 'inline' : ''}`} viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" /> </svg>
     )
 }
 
